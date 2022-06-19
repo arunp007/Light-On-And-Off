6465a9ba3f48a4fe13a2eb693e21b04470c3bdb5
arunp007 <arunp4624@gmail.com>
2022-06-19 21:37:28 +0530

    Added Some changes

---
+++ lightproject/lightapp/static/javascript/script.js
@@ -0,0 +1,20 @@
+function light(sw){
+    var pic
+    var bg = document.getElementById('bg')
+    if(sw == 0){
+        pic = "https://i.ibb.co/4gLYqqb/off.png"
+    }
+
+    else{
+        pic = "https://i.ibb.co/z7gX2GX/on.png"
+
+    }
+
+    if(sw == 1){
+        bg.style.backgroundColor="black"
+    }
+    if(sw == 0){
+        bg.style.backgroundColor="white"
+    }
+    document.getElementById('blub').src = pic
+}
